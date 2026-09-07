c06f4779f5ae4c20b0138cf0a2972c97db2d253e
daniel <danielolsen030619@gmail.com>
2026-09-07 13:55:03 +0200

    tilføjet hero billede & overskrift boks

--- js/index.js
+++ js/index.js
@@ -6,14 +6,51 @@
 
 let header = document.querySelector(".header")
 
-let createHeaderDiv = createElement("div")
-let createMenu = createElement("img")
+let createHeaderDiv = document.createElement("div")
+createHeaderDiv.classList.add("headerDiv")
+
+let createMenu = document.createElement("img")
 createMenu.setAttribute("src", "https://placehold.co/32x32")
 createMenu.setAttribute("alt", "Menu")
+createMenu.classList.add("headerMenu")
+
 createHeaderDiv.append(createMenu)
 
 let createIcon = document.createElement("img");
 createIcon.setAttribute("src", "assets/cube_1.svg");
 createIcon.setAttribute("alt", "BIGCAT");
+createIcon.classList.add("headerIcon")
 
 header.append(createIcon, createHeaderDiv);
+
+
+// Hero
+let elementHero = document.querySelector(".hero")
+
+let createHeroImg = document.createElement("img")
+createHeroImg.setAttribute("src", hero.image)
+createHeroImg.setAttribute("alt", "Hero")
+createHeroImg.classList.add("heroImg")
+
+let createHeroDiv = document.createElement("div")
+createHeroDiv.classList.add("heroDiv")
+
+let createH1 = document.createElement("h1")
+createH1.textContent = hero.headline
+
+let createHeroP = document.createElement("p")
+createHeroP.textContent = hero.copy
+
+let createHeroBtn = document.createElement("a")
+let createHeroBtnP = document.createElement("p")
+createHeroBtnP.textContent = "Explore"
+let createHeroBtnIcon = document.createElement("img")
+createHeroBtnIcon.setAttribute("src", hero.icon)
+createHeroBtnIcon.setAttribute("alt", "Button globe icon")
+createHeroBtn.classList.add("heroBtn")
+
+createHeroBtn.append(createHeroBtnIcon, createHeroBtnP)
+
+createHeroDiv.append(createH1, createHeroP, createHeroBtn)
+
+elementHero.append(createHeroDiv, createHeroImg)
