0c57262c352543e458346c0623966cba7ab06e65
daniel <danielolsen030619@gmail.com>
2026-09-07 11:12:13 +0200

    tilføjet header ikoner

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 // slå dig løs her... 
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+
+// header
+
+let header = document.querySelector(".header")
+
+let createHeaderDiv = createElement("div")
+let createMenu = createElement("img")
+createMenu.setAttribute("src", "https://placehold.co/32x32")
+createMenu.setAttribute("alt", "Menu")
+createHeaderDiv.append(createMenu)
+
+let createIcon = document.createElement("img");
+createIcon.setAttribute("src", "assets/cube_1.svg");
+createIcon.setAttribute("alt", "BIGCAT");
+
+header.append(createIcon, createHeaderDiv);
